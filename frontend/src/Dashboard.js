5808f574641dfe6dbce46224efbc5f9bdf11b2bf
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2024-12-02 18:26:08 +0530

    Update Dashboard.js

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -7,7 +7,7 @@ function Dashboard({ token, setToken }) {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://127.0.0.1:9000/dashboard", {
+      const response = await axios.get("http://localhost:9000/dashboard", {
         headers: { "x-access-token": token },
       });
       setMessage(response.data.message);
