379cc2303db9710c1bacf18b1f11adafcb0c7ee6
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2024-12-02 18:25:55 +0530

    Update LoginForm.js

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -10,7 +10,7 @@ function LoginForm({ setToken }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://127.0.0.1:9000/login/', {
+      const response = await axios.post('http://localhost:9000/login/', {
         username: username,
         password: password,
       }, {
